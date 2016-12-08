9d25dc288515a39b5d2db13c0f042a739e704abe
Aibar Nurlanov <aibar.nurlanov@gmail.com>
2016-12-08 23:04:15 +0600

    +Headers.get

--- src/walkingdevs/http11/Headers.java
+++ src/walkingdevs/http11/Headers.java
@@ -3,6 +3,8 @@ package walkingdevs.http11;
 public interface Headers extends Iterable<Header> {
     boolean has(String header);
 
+    Header get(String name);
+
     Headers add(String name, String value);
 
     Headers del(String name);
