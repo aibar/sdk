8ea20d8eee19e06898ee643071362f9e1c5c4018
Aibar Nurlanov <aibar.nurlanov@gmail.com>
2017-09-08 18:51:36 +0600

    fixed available

--- src/walkingdevs/stream/$BufferedIs.java
+++ src/walkingdevs/stream/$BufferedIs.java
@@ -1,23 +1,21 @@
 package walkingdevs.stream;
 
-import walkingdevs.Problems;
 import walkingdevs.val.$Val;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 public class $BufferedIs {
+    public static BufferedIs mk() {
+        return new BufferedIsEmpty();
+    }
+
     public static BufferedIs mk(InputStream is) {
         return mk(is, 8192);
     }
 
     public static BufferedIs mk(InputStream is, int size) {
-        try {
-            if (is == null || is.available() < 1) {
-                return new BufferedIsEmpty();
-            }
-        } catch (IOException fail) {
-            throw Problems.weFucked(fail);
+        if (is == null) {
+            return mk();
         }
         return new BufferedIsImpl(
             is,
