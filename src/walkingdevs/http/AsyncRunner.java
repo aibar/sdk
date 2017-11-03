fd8bf1fc1461bc13448b75ae45af9e60ce97bb2e
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-11-03 16:42:36 +0600

    thread

--- src/walkingdevs/http/AsyncRunner.java
+++
@@ -1,9 +0,0 @@
-package walkingdevs.http;
-public interface AsyncRunner {
-
-    void closeAll();
-
-    void closed(ClientHandler clientHandler);
-
-    void exec(ClientHandler code);
-}
--- src/walkingdevs/http/Exo.java
+++ src/walkingdevs/http/Exo.java
@@ -2,75 +2,73 @@ package walkingdevs.http;
 
 import walkingdevs.fun.Action;
 import walkingdevs.fun.Function;
-import walkingdevs.tcp.Tcp;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 class Exo implements Http.Server {
     public void start() {
-        loopThread.setDaemon(await);
         loopThread.start();
-        while (loopThread.isInterrupted())
-        {
-            try {
-                Thread.sleep(10L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
         success.run();
     }
 
     public boolean isAlive() {
-        return false;
+        return loopThread.isAlive();
     }
 
     public void kill() {
-        loopThread.isInterrupted();
+        loopThread.interrupt();
     }
 
     Exo(Host host, Port port, Function<HttpResponse, HttpRequest> handler, Action success, boolean await) {
         this.success = success;
         this.await = await;
-        int i = 0;
-        loopThread = new Thread(() -> {
-            ServerSocket server;
+        loopThread = new Thread(()->{
             try {
                 server = new ServerSocket();
                 server.setReuseAddress(true);
-                server.bind(
-                    new InetSocketAddress(
-                        host.inet(),
-                        port.get()
-                    )
-                );
-                System.out.println("Exo started...");
-            } catch (IOException e) {
-                throw new RuntimeException(
-                    e
-                );
-            }
-            do {
-                try (Socket client = server.accept()) {
-                    asyncRunner.exec(
-                        new ClientHandler(handler, client)
-                    );
-                } catch (IOException e) {
-                    e.printStackTrace();
+                server.bind(new InetSocketAddress(
+                    host.inet(),
+                    port.get()
+                ));
+                System.out.println("Server started....");
+                while (!server.isClosed()){
+                    Socket client = server.accept();
+                    new Thread(()->{
+                        try{
+                            OutputStream os = client.getOutputStream();
+                            handler.run(HttpRequest.mk()).writeFormattedTo(os);
+                            Thread.sleep(1000);
+                            os.flush();
+                            client.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                client.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
                 }
-            } while (!server.isClosed());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
     }
 
     private final Action success;
     private boolean await;
+    private ServerSocket server;
     private final Thread loopThread;
-    protected AsyncRunner asyncRunner;
 
     public static void main(String[] args) {
-        Tcp.server().build().start();
+        Http.server().build().start();
     }
 }
