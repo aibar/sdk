37deb9bd3c56f631e69805ebf92d373dd0ba8434
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-11-07 17:45:16 +0600

    action

--- src/walkingdevs/http/Exo.java
+++ src/walkingdevs/http/Exo.java
@@ -11,24 +11,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 class Exo implements Http.Server {
-    public void start() {
-        success.run();
-    }
-
-    public boolean isAlive() {
-        return threadPool.isShutdown();
-    }
-
-    public void kill() {
-        threadPool.shutdown();
-    }
-
-    Exo(Host host, Port port, Function<HttpResponse, HttpRequest> handler, Action success, boolean await) {
-        this.success = success;
-        this.await = await;
+    public synchronized void start() {
         try {
             server = new ServerSocket();
-            server.bind(new InetSocketAddress(host.inet(), port.get()));
+            server.bind(new InetSocketAddress(this.host.inet(), this.port.get()));
             System.out.println("Server started....");
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,7 +25,7 @@ class Exo implements Http.Server {
                 Socket client = server.accept();
                 threadPool.submit(()->{
                     try {
-                        handler.run(HttpRequest.mk()).writeFormattedTo(client.getOutputStream());
+                        this.handler.run(HttpRequest.mk()).writeFormattedTo(client.getOutputStream());
                         Thread.sleep(10L);
                         client.close();
                     } catch (IOException e) {
@@ -57,12 +43,33 @@ class Exo implements Http.Server {
         } finally {
             threadPool.shutdown();
         }
+        success.run();
+    }
+
+    public synchronized boolean isAlive() {
+        return threadPool.isShutdown();
+    }
+
+    public synchronized void kill() {
+        threadPool.shutdown();
+    }
+
+    Exo(Host host, Port port, Function<HttpResponse, HttpRequest> handler, Action success, boolean await) {
+        this.host = host;
+        this.port = port;
+        this.handler = handler;
+        this.success = success;
+        this.await = await;
     }
     private final Action success;
     private boolean await;
     private ServerSocket server;
     private ExecutorService threadPool;
+    private Host host;
+    private Port port;
+    private Function<HttpResponse, HttpRequest> handler;
     public static void main(String[] args) {
         Http.server().build().start();
     }
+
 }
