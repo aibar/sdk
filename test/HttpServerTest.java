91e8325965b60a82bbb238a89215aaf370733341
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-11-09 05:13:45 +0600

    back

--- test/HttpServerTest.java
+++ test/HttpServerTest.java
@@ -5,20 +5,19 @@ import walkingdevs.http.ReqBuilder;
 
 public class HttpServerTest extends Assert {
     @Test
-    public void shouldStart() throws InterruptedException {
+    public void shouldStart() {
         Http.server()
             .success(() -> {
+                assertEquals(
+                    "Exo server is up and running.",
+                    ReqBuilder.GET("http://localhost:8080")
+                        .build()
+                        .send()
+                        .body()
+                        .text()
+                );
             })
             .build()
             .start();
-        Thread.sleep(10000);
-        assertEquals(
-            "Exo server is up and running.",
-            ReqBuilder.GET("http://localhost:8080")
-                .build()
-                .send()
-                .body()
-                .text()
-        );
     }
 }
