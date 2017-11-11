a05cd5055826f775e0512aa5a4e9486ce3818a3e
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-11-11 16:17:44 +0600

    IO

--- src/walkingdevs/http/Exo.java
+++ src/walkingdevs/http/Exo.java
@@ -3,7 +3,7 @@ package walkingdevs.http;
 import walkingdevs.fun.Action;
 import walkingdevs.fun.Function;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -39,7 +39,28 @@ class Exo implements Http.Server {
                     Socket clientSocket = serverSocket.accept();
                     clientThread = new Thread(()->{
                         try {
-                            handler.run(HttpRequest.mk()).writeFormattedTo(clientSocket.getOutputStream());
+                            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                            String s;
+                            while ((s=in.readLine())!=null){
+                                System.out.println(s);
+                                if (s.isEmpty()){
+                                    break;
+                                }
+                            }
+                            out.write("HTTP/1.0 200 OK\r\n");
+                            out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+                            out.write("Server: Apache/0.8.4\r\n");
+                            out.write("Content-Type: text/html\r\n");
+                            out.write("Content-Length: 59\r\n");
+                            out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
+                            out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+                            out.write("\r\n");
+                            out.write("Exo server is up and running.");
+                            out.close();
+                            in.close();
+                            clientSocket.close();
+                            //handler.run(HttpRequest.mk()).writeFormattedTo(clientSocket.getOutputStream());
                         } catch (IOException e) {
                             e.printStackTrace();
                         } finally {
