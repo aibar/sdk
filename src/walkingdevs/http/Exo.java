0c7690ad8c5044cb7a205d30546603cb8fc33d31
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-11-11 17:43:11 +0600

    factory

--- src/walkingdevs/http/Exo.java
+++ src/walkingdevs/http/Exo.java
@@ -3,8 +3,10 @@ package walkingdevs.http;
 import walkingdevs.fun.Action;
 import walkingdevs.fun.Function;
 
-import java.io.*;
-import java.net.InetSocketAddress;
+import javax.net.ServerSocketFactory;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -28,8 +30,7 @@ class Exo implements Http.Server {
         serverThread = new Thread(()->{
             ServerSocket serverSocket = null;
             try {
-                serverSocket = new ServerSocket();
-                serverSocket.bind(new InetSocketAddress(host.inet(), port.get()));
+                serverSocket = ServerSocketFactory.getDefault().createServerSocket(port.get());
                 System.out.println("Server started on port: " + serverSocket.getLocalPort() + "\n");
             } catch (IOException e) {
                 e.printStackTrace();
